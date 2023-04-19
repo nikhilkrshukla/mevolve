1a69a070b8ea7df705b66cb983cb476ed558eeb4
nikhilkrshukla@gmail.com <nikhilkrshukla@gmail.com>
2023-04-20 02:07:43 +0530

    Minor Fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
+import { HeroBannerComponent } from './component/hero-banner/hero-banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HeroBannerComponent
   ],
   imports: [
     BrowserModule,
